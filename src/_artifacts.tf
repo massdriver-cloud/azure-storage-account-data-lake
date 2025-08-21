ebf628de4d2c03dde97f24d7194e9ea83e6a9360
Cory O'Daniel <cory@coryodaniel.com>
2025-08-21 11:52:02 -0700

    OCI Support (#9)
    * OCI Support
    
    * removing type
    
    * remove deprecated field
    
    ---------
    
    Co-authored-by: chrisghill <chris@massdriver.cloud>

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,7 +1,6 @@
 resource "massdriver_artifact" "azure_storage_account_data_lake" {
-  field                = "azure_storage_account_data_lake"
-  provider_resource_id = module.azure_storage_account.account_id
-  name                 = "Azure Data Lake Storage Account ${var.md_metadata.name_prefix} (${module.azure_storage_account.account_id})"
+  field    = "azure_storage_account_data_lake"
+  name     = "Azure Data Lake Storage Account ${var.md_metadata.name_prefix} (${module.azure_storage_account.account_id})"
   artifact = jsonencode(
     {
       data = {
